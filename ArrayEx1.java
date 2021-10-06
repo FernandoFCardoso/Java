0f284f135e2ce878663a55ac0d784ee050257423
FernandoFCardoso <advfernandofcardoso@gmail.com>
2021-10-06 17:26:15 -0300

    Exercícios de Array Vetores e Matrizes

---
+++ ArrayEx1.java
@@ -0,0 +1,29 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class ArrayEx1 {
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int A[]= {1, 0, 5, -2, -5, 7};
+		int soma;
+		
+		
+					
+		A[3] = 100;
+		
+		
+		soma=(A[0] + A[1] + A[5]);
+		
+		System.out.println("\n Os valores dos vetores são: \n" + A[0]+"\n"+A[1]+"\n"+A[2]+"\n"+A[3]+"\n"+A[4]+"\n"+A[5]);
+		System.out.println("\n A soma das linhas é de: "+soma);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Arrayex3.java
@@ -0,0 +1,34 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Arrayex3 {
+	
+	public static void main(String[] args) {
+		
+		
+				
+		int M[][] = new int[3][3];
+		int x=0, linha, coluna;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for (linha=0; linha<3; linha++)
+		{
+			for (coluna=0; coluna<3; coluna++)
+			{
+				System.out.println("\n: Entre com um número: ");
+				M[linha][coluna] = leia.nextInt();
+				
+				if(M[linha][coluna]>=10)
+			     {
+			    	 x++;
+			     }
+			}
+		}
+		
+		System.out.printf("\n: Os números a mais de 10 são: %d ",x);
+		     
+	}
+}
+
